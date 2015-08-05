e6491eb290a8f66e834d1fb9c9e5eb986d06ba93
Vitor Capela <dodecaphonic@gmail.com>
2015-08-04 23:43:48 -0300

    Shoots many lasers.

--- src/Spaceship.elm
+++ src/Spaceship.elm
@@ -9,8 +9,20 @@ import Time exposing (fps)
 import Keyboard
 
 
+type alias WindowHeight = Float
+
+
+type alias Shooting = Bool
+
+
+type alias Shot = { x: Float
+                  , y: Float
+                  }
+
+
 type alias Model = { x : Float
                    , y : Float
+                   , shots : List Shot
                    }
 
 
@@ -33,6 +45,10 @@ laserImage =
     image w h "sprites/laser.png"
 
 
+laserVelocity : Float
+laserVelocity = 10.0
+
+
 spaceshipImage : Element
 spaceshipImage =
   let
@@ -41,21 +57,65 @@ spaceshipImage =
     image w h "sprites/player.png"
 
 
-glide : Float -> Keys -> Model -> Model
-glide dt keys spaceship =
-  { spaceship | x <- spaceship.x + dt * (toFloat keys.x) }
-
-
 spaceship : Model
 spaceship = { x = 0
             , y = 0
+            , shots = []
             }
 
 
-update : (Float, Keys) -> Model -> Model
-update (dt, keys) spaceship =
+update : (Float, Keys, Shooting, WindowHeight) -> Model -> Model
+update (dt, arrows, shooting, wh) spaceship =
   spaceship
-    |> glide dt keys
+    |> glide dt arrows wh
+    |> shoot shooting wh
+    |> updateShots wh
+
+
+glide : Float -> Keys -> WindowHeight -> Model -> Model
+glide dt arrows wh spaceship =
+  let
+    (_, sh) =
+      spaceshipDimensions
+
+    spaceshipY =
+      (toFloat sh) - wh / 2
+  in
+    { spaceship | x <- spaceship.x + dt * (toFloat arrows.x)
+                , y <- spaceshipY
+    }
+
+
+shoot : Shooting -> WindowHeight -> Model -> Model
+shoot shooting wh spaceship =
+  let
+    (_, lh) =
+      laserDimensions
+
+    laserY =
+      (toFloat lh) + spaceship.y
+  in
+    if shooting then
+      { spaceship | shots <- { x = spaceship.x, y = laserY } :: spaceship.shots }
+    else
+      spaceship
+
+
+updateShots : WindowHeight -> Model -> Model
+updateShots wh spaceship =
+  let
+    updateShot s = { s | y <- s.y + laserVelocity }
+    updated      = List.map updateShot spaceship.shots
+    inScreen     = List.filter (\s -> s.y < (wh / 2)) updated
+  in
+    { spaceship | shots <- inScreen }
+
+
+renderShot : Float -> Float -> Shot -> Form
+renderShot w h shot =
+  laserImage
+    |> toForm
+    |> move (shot.x, shot.y)
 
 
 gameStage : (Int, Int) -> Model -> Element
@@ -64,44 +124,43 @@ gameStage (w', h') spaceship =
     (w, h) =
       (toFloat w', toFloat h')
 
-    (sw, sh) =
+    (_, sh) =
       spaceshipDimensions
 
     (_, lh) =
       laserDimensions
 
-    spaceshipY =
-      (toFloat sh) - h / 2
-
     laserY =
-      (toFloat lh) - h / 2 + (toFloat sh) + 10
+      (toFloat lh) - h / 2 + 10
 
     spaceshipPosition =
-      (spaceship.x, spaceshipY)
+      (spaceship.x, spaceship.y)
 
     laserPosition =
       (spaceship.x, laserY)
   in
     layers [
       collage w' h'
-        [ rect w h
+        ([ rect w h
             |> filled black
+        , laserImage
+          |> toForm
+            |> move laserPosition
         , spaceshipImage
             |> toForm
             |> move spaceshipPosition
-        , laserImage
-            |> toForm
-            |> move laserPosition
-        ]
+        ] ++ (List.map (renderShot w h) spaceship.shots))
     ]
 
 
-input : Signal (Float, Keys)
+input : Signal (Float, Keys, Shooting, WindowHeight)
 input =
   let
     delta = Signal.map (\t -> t / 5) (fps 30)
+    asTuple = (\dt a s h -> (dt, a, s, (toFloat h)))
+    inputs = Signal.map4 asTuple delta Keyboard.arrows Keyboard.space Window.height
   in
-    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
+    Signal.sampleOn delta inputs
 
 
 main : Signal Element
