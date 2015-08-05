dc07b6b361b63b1d8577ff942e2359995040abd5
Vitor Capela <dodecaphonic@gmail.com>
2015-08-04 22:27:09 -0300

    Moves ship from side to side.

--- src/Spaceship.elm
+++ src/Spaceship.elm
@@ -5,24 +5,81 @@ import Window
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
+import Time exposing (fps)
+import Keyboard
 
 
-playerImage =
-  image 50 52 "sprites/player.png"
+type alias Model = { x : Float
+                   , y : Float
+                   }
 
-gameStage : (Int, Int) -> Element
-gameStage (w, h) =
-  layers [
-    collage w h
-      [ rect (toFloat w) (toFloat h)
-          |> filled lightBlue
-      , playerImage
-          |> toForm
-          |> move (((toFloat w) / 2.0) - 25.0) ((toFloat h) - 26.0)
-      ]
-  ]
+
+type alias Keys = { x : Int, y : Int }
+
+
+spaceshipDimensions : (Int, Int)
+spaceshipDimensions = (50, 52)
+
+
+spaceshipImage : Element
+spaceshipImage =
+  let
+    (w, h) = spaceshipDimensions
+  in
+    image w h "sprites/player.png"
+
+
+glide : Float -> Keys -> Model -> Model
+glide dt keys spaceship =
+  { spaceship | x <- spaceship.x + dt * (toFloat keys.x) }
+
+
+spaceship : Model
+spaceship = { x = 0
+            , y = 0
+            }
+
+
+update : (Float, Keys) -> Model -> Model
+update (dt, keys) spaceship =
+  spaceship
+    |> glide dt keys
+
+
+gameStage : (Int, Int) -> Model -> Element
+gameStage (w', h') spaceship =
+  let
+    (w, h) =
+      (toFloat w', toFloat h')
+
+    (_, sh) =
+      spaceshipDimensions
+
+    spaceshipY =
+      (toFloat sh) - h / 2
+
+    position =
+      (spaceship.x, spaceshipY)
+  in
+    layers [
+      collage w' h'
+        [ rect w h
+            |> filled black
+        , spaceshipImage
+            |> toForm
+            |> move position
+        ]
+    ]
+
+
+input : Signal (Float, Keys)
+input =
+  let
+    delta = Signal.map (\t -> t / 5) (fps 30)
+  in
+    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
 
 
 main : Signal Element
 main =
-  Signal.map gameStage Window.dimensions
+  Signal.map2 gameStage Window.dimensions (Signal.foldp update spaceship input)
